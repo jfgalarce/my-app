b1c5e00fa7aec82e4f95080f12b7e27abb8af47d
jfgalarce <jfgalarce@gmail.com>
2025-04-09 21:22:45 +0000

    primer proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'juan galarce';
+  data = '222322'
 }
